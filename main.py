868a27bf39c1f28ade8dee32333e049b12ccd78c
Soew <55360932+Lousio-n@users.noreply.github.com>
2024-06-19 20:08:56 +0200

    create function for printing out report

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def main():
     file_path = "books/frankenstein.txt"
     text = read_file(file_path)
-    print(count_words(text))
-    print(count_chars(text))
+    num_words = count_words(text)
+    char_list = dict_converter(count_chars(text))
+    print(f"--- start report of {file_path} ---")
+    print(f"{num_words} was found")
+    for x in char_list:
+        print(f"The char {x['char']} was found {x['val']} times")
+    print("--- end of report ---")
 
 
 def count_words(text):
@@ -20,11 +25,25 @@ def count_chars(text):
     chars = {}
     lower_text = text.lower()
     for char in lower_text:
+        if not char.isalpha():
+            continue
         if char in chars:
             chars[char] += 1
         else:
             chars[char] = 1
     return chars
+
+
+def get_val(dict):
+    return dict["val"]
+
+
+def dict_converter(dict):
+    lst = []
+    for key in dict:
+        lst.append({"char": key, "val": dict[key]})
+    lst.sort(key=get_val, reverse=True)
+    return lst
 
 
 main()
